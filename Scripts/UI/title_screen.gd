f4af738ed61e2e8e5b1e67805f292bcbf52cbc92
Joan Prim Armengol <jonpri1995@hotmail.com>
2024-11-05 14:36:59 +0100

    Title screen with three buttons (Start Game, Options and Leaderboard)
    Exports the scenes to load when pressing the buttons

---
+++ Scripts/UI/title_screen.gd
@@ -0,0 +1,30 @@
+extends Control
+
+var start : Button = null
+var options : Button = null
+var leaderboard : Button = null
+
+## Scene to switch to when Start Game is pressed
+@export var game_scene : PackedScene
+## Scene to switch to when Options is pressed
+@export var options_scene : PackedScene
+## Scene to switch to when Leaderboard is pressed
+@export var leaderboard_scene :  PackedScene
+
+func _ready():
+	start = find_child("StartGame")
+	start.pressed.connect(_on_start_game)
+	options = find_child("Options")
+	options.pressed.connect(_on_options)
+	leaderboard = find_child("Leaderboard")
+	leaderboard.visible = false # TODO: Set this based on the save file data
+	leaderboard.pressed.connect(_on_leaderboard)
+
+func _on_start_game():
+	get_tree().change_scene_to_packed(game_scene)
+	
+func _on_options():
+	get_tree().change_scene_to_packed(options_scene)
+	
+func _on_leaderboard():
+	get_tree().change_scene_to_packed(leaderboard_scene)
