015347e16b236d692a9907be901ce43b77b9c70e
Joan Prim Armengol <jonpri1995@hotmail.com>
2024-11-06 10:04:03 +0100

    Scale window size based on the scaling factor

--- Scripts/UI/display_options.gd
+++ Scripts/UI/display_options.gd
@@ -3,13 +3,13 @@ extends Node
 
 ## The arrays of items for the scale factor
 ## Populates the item list
-@export var items : Array = ["0.8", "1.3"]
+@export var items : Array[float]
 
 func _ready():
 	for item in items:
-		item_list.add_item(item)
+		item_list.add_item("%.02f" % item)
 	#item_list.select(int(get_window().content_scale_factor)-1)
 
-## Update the scale factor for the window
+## Multiply the window resolution by the scale factor
 func _on_item_list_item_selected(index):
-	get_window().size = get_window().size * items[index].to_float()
+	get_window().size = Constants.BASE_RESOLUTION * items[index]
