7e0dd1fb2b3b41487b5001b6d30853d1e3dfc645
Joan Prim Armengol <jonpri1995@hotmail.com>
2024-11-06 09:48:36 +0100

    Populate the item list from an array and set viewport size by multiplying the current size

--- Scripts/UI/display_options.gd
+++ Scripts/UI/display_options.gd
@@ -1,9 +1,15 @@
 extends Node
 @onready var item_list = $ItemList
 
+## The arrays of items for the scale factor
+## Populates the item list
+@export var items : Array = ["0.8", "1.3"]
+
 func _ready():
-	item_list.select(int(get_window().content_scale_factor)-1)
+	for item in items:
+		item_list.add_item(item)
+	#item_list.select(int(get_window().content_scale_factor)-1)
 
 ## Update the scale factor for the window
 func _on_item_list_item_selected(index):
-	get_window().content_scale_factor = float(index+1)
+	get_window().size = get_window().size * items[index].to_float()
