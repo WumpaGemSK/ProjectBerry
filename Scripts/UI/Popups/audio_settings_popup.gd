62aa53928c2ca34ad5c1007423bb11a092c38f07
Joan Prim Armengol <jonpri1995@hotmail.com>
2024-11-07 17:27:11 +0100

    Ingame menu with the resume, audio settings and display settings hooked up.
    Audio doesn't work and the display popup is placed on top of the ingame menu.

---
+++ Scripts/UI/Popups/audio_settings_popup.gd
@@ -0,0 +1,10 @@
+extends Control
+class_name AudioSettingsPopup
+
+@onready var popup_panel = $PopupPanel
+
+func popup():
+	popup_panel.popup_centered(popup_panel.min_size)
+
+func hide_popup():
+	popup_panel.hide()
---
+++ Scripts/UI/Popups/in_game_menu.gd
@@ -0,0 +1,42 @@
+extends PopupPanel
+
+@onready var display_options_popup = $DisplayOptionsPopup
+@onready var audio_settings_popup = $AudioSettingsPopup
+
+var sub_menu_open : bool = false
+
+func _ready():
+	display_options_popup.popup_panel.popup_hide.connect(hide_display_popup)
+	audio_settings_popup.popup_panel.popup_hide.connect(hide_audio_popup)
+	
+func _on_close():
+	display_options_popup.hide_popup()
+	audio_settings_popup.hide_popup()
+	audio_settings_popup.visible = false
+	display_options_popup.visible = false
+	sub_menu_open = false
+
+func _process(_delta):
+	if Input.is_action_pressed("options") and !sub_menu_open:
+		if !self.visible:
+			popup_centered(size)
+
+func _on_resume_pressed():
+	hide()
+
+func _on_display_pressed():
+	sub_menu_open = true
+	display_options_popup.visible = true
+	display_options_popup.popup()	
+
+
+func _on_audio_pressed():
+	sub_menu_open = true
+	audio_settings_popup.visible = true
+	audio_settings_popup.popup()
+
+func hide_audio_popup():
+	audio_settings_popup.visible = false
+
+func hide_display_popup():
+	display_options_popup.visible = false
