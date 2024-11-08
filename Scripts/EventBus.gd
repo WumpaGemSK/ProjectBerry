76e1596a9f76fe3b5f59264b4cb3b29f36ab1c0d
Joan Prim Armengol <jonpri1995@hotmail.com>
2024-11-08 16:53:24 +0100

    Add Eventbus autoload

---
+++ Scripts/EventBus.gd
@@ -0,0 +1,8 @@
+extends Node
+## Class to hold global signals to emit/connect to
+
+## Signal to listen for the pause notification
+signal pause
+
+## Signal to listen for the resume notification
+signal resume
