ba2c948ad1d3b94982cbb81c73a1ec176647de7d
Joan Prim Armengol <jonpri1995@hotmail.com>
2024-11-08 17:22:14 +0100

    Change display options input

--- Scripts/UI/display_options.gd
+++ Scripts/UI/display_options.gd
@@ -1,16 +1,32 @@
 extends Control
-@onready var item_list = $HSplitContainer/ItemList
+
+@onready var value_label = %Value
 
 ## The arrays of items for the scale factor
 ## Populates the item list
 @export var items : Array[float]
 
+var index = 0
 ## Populate the list with the possible values
 func _ready():
-	for item in items:
-		item_list.add_item("%.02f" % item)
-	item_list.grab_focus()
+	#TODO: Find a way to preselect the option
+	update_label(items[index])
 
 ## Emit the signal to change the viewport size with the factor as a parameter
 func _on_item_list_item_selected(index):
 	Settings._viewport_change_size.emit(items[index])
+
+func _on_left_pressed():
+	index = clamp(index-1, 0, items.size()-1)
+	var factor = items[index]
+	update_label(factor)
+	Settings._viewport_change_size.emit(factor)
+
+func _on_right_pressed():
+	index = clamp(index+1, 0, items.size()-1)
+	var factor = items[index]
+	update_label(factor)
+	Settings._viewport_change_size.emit(factor)
+
+func update_label(number: float):
+	value_label.text = "x%.01f" % number
