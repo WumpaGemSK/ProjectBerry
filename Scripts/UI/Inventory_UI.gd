55da357a71958d6797d22b7770645ad5e0070d43
Joan Prim Armengol <jonpri1995@hotmail.com>
2024-11-11 17:43:49 +0100

    Toggle inventory when the "inventory" action is triggered

--- Scripts/UI/Inventory_UI.gd
+++ Scripts/UI/Inventory_UI.gd
@@ -1,18 +1,20 @@
-extends Node
+extends Control
 
 @export var player : Player
 @export var item_slot : PackedScene
-@onready var grid_container = $PanelContainer/VBoxContainer/HBoxContainer/GridContainer
+@onready var grid_container : GridContainer = %InventoryGrid
 
-var inventory : Array[Item] = []
+var inventory : Array[ItemSlot] = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	EventBus.pick_item.connect(on_item_pickup)
-	#inventory_updated()
+	hide()
+	inventory_updated()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("inventory"):
+		visible = !visible
 
 func on_item_pickup(item: Item):
 	for child in grid_container.get_children():
@@ -21,22 +23,21 @@ func on_item_pickup(item: Item):
 			i.quantity += 1
 			return
 	var slot : ItemSlot = item_slot.instantiate()
+	slot.name = item.item_name
 	grid_container.add_child(slot)
 	slot.quantity = 1
 	slot.item_name = item.item_name
 	slot.effect = item.effect
 	slot.type = item.type
 	slot.icon.texture = item.texture_icon
-	#inventory_updated()
+	inventory.append(slot)
+	inventory_updated()
 
 func inventory_updated():
 	for child in grid_container.get_children():
-		child.queue_free()
+		pass#child.queue_free()
 	for item in inventory:
-		var slot : ItemSlot = item_slot.instantiate()
-		slot.quantity = 1
-		grid_container.add_child(slot)
-	
+		grid_container.add_child(item)
 	while grid_container.get_child_count() < 16:
 		var slot = item_slot.instantiate()
 		grid_container.add_child(slot)
