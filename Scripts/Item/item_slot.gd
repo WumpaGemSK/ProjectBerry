47d4348c4b86fe6e69db2bd1854c1a463c0a36c4
Joan Prim Armengol <jonpri1995@hotmail.com>
2024-11-11 19:40:02 +0100

    Move quantity to Base class and setter to copy the data from an item

--- Scripts/Item/item_slot.gd
+++ Scripts/Item/item_slot.gd
@@ -4,22 +4,33 @@ class_name ItemSlot
 @onready var icon = %Icon
 @onready var count = %Count
 @onready var count_block = %Count_Block
-@onready var button = $PanelContainer/Button
+@onready var button = $Button
 
-var quantity :int = 0:
-	set(value):
-		quantity = value
-		count.text = "%d" % quantity
-		count_block.visible = quantity > 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	icon.texture = texture_icon
 	count_block.visible = quantity > 1
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
 
+func set_item(item: Item):
+	icon.texture = item.texture_icon
+	quantity = item.quantity
+	item_name = item.item_name
+	effect = item.effect
+	type = item.type
+	update_count_label()
+
+func set_empty():
+	quantity = 0
+	icon.texture = null
+	type = Item.Item_type.NONE
+
 func grab_focus():
 	button.grab_focus()
+
+func update_count_label():
+	count.text = "%d" % quantity
+	count_block.visible = quantity > 1
