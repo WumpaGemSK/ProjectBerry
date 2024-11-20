2e6b48f03e9b207909e59744d274c71de4298639
Joan Prim Armengol <jonpri1995@hotmail.com>
2024-11-20 11:34:02 +0100

    Rename variable to avoid name conflicts

--- Scripts/weapon.gd
+++ Scripts/weapon.gd
@@ -8,7 +8,7 @@ signal hit
 ## How much time in seconds before attacking againg with the weapon
 @export var cooldown : float = 1.0
 ## Range of the weapon in pixels
-@export var range: float
+@export var weapon_range: float
 ## Damage caused by on attack of the weapon
 @export var damage: int
 ## Is the weapon melee?
@@ -24,11 +24,11 @@ func attack(_from: Vector2, _direction: Vector2):
 	return
 
 func raycast_to_damageable(origin: Vector2, dir: Vector2):
-	var dest : Vector2 = origin + dir*range
+	var dest : Vector2 = origin + dir*weapon_range
 	var space_state = get_world_2d().direct_space_state
 	var query = PhysicsRayQueryParameters2D.create(origin, dest)
 	var result = space_state.intersect_ray(query)
-	return result.collider if result.size() > 0 and result.position.distance_to(origin) < range else null
+	return result.collider if result.size() > 0 and result.position.distance_to(origin) < weapon_range else null
 
 func reload(_amount: int) -> bool:
 	return false
