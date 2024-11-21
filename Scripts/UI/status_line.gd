482f11fb60c7b3a6d8e067352c0fb6cd41ef312a
Joan Prim Armengol <jonpri1995@hotmail.com>
2024-11-21 18:29:05 +0100

    Trying to fill the space in the middle of each status line

--- Scripts/UI/status_line.gd
+++ Scripts/UI/status_line.gd
@@ -1,9 +1,20 @@
 extends Control
 class_name StatusLine
 
-@onready var field = $MarginContainer/Field
-@onready var data = $MarginContainer/Data
+@onready var field = %Field
+@onready var filler = %Filler
+@onready var data = %Data
 
 func set_data(field_name: String, data_text: String):
 	field.text = field_name
 	data.text = data_text
+	call_deferred("set_filler")
+
+func set_filler():
+	var field_width = field.size.x
+	var data_width = data.size.x
+	var s = size.x
+	var count: int = s - field_width - data_width
+	filler.position.x += field_width
+	filler.size.x -= field_width
+	filler.text = "*   ".repeat(count/14)
