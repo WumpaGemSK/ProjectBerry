6b207c2f486f940a7d1a150f3bcc03e2bcf31eca
Joan Prim Armengol <jonpri1995@hotmail.com>
2024-11-23 13:12:25 +0100

    Use Utils.format_time instead of local implementation

--- Scripts/UI/HUD/countdown.gd
+++ Scripts/UI/HUD/countdown.gd
@@ -16,15 +16,8 @@ func _ready():
 	tween.tween_property(self, "modulate:a", 1, Constants.COUNDTDOWN_FADE_IN)
 	tween.tween_callback(func(): EventBus.countdown_start.emit())
 
-## Format the time left as minutes, seconds, miliseconds. Time left is in seconds
-func format_time(time_left: float) -> String:
-	var minutes : int = int(time_left / 60)
-	var seconds : int = int(fmod(time_left, 60))
-	var miliseconds : int = int(fmod(time_left, 1) * 100)
-	return "%02d:%02d.%02d" % [minutes, seconds, miliseconds]
-
 func update_label(time_left: float):
-	label.text = format_time(time_left)
+	label.text = Utils.format_time(time_left)
 
-func _process(delta):
+func _process(_delta):
 	update_label(timer.time_left)
