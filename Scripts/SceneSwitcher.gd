574f4dea6a8d46fae4625cccba1a9985d8d634d8
Joan Prim Armengol <jonpri1995@hotmail.com>
2024-11-24 12:11:33 +0100

    Check if new scene has a spawn point and block the change and print a message if not

--- Scripts/SceneSwitcher.gd
+++ Scripts/SceneSwitcher.gd
@@ -6,14 +6,17 @@ var prev_position: Vector2
 var player: Player
 
 func change_scene(scene: PackedScene, trigger: Node2D):
+	var new_scene = scene.instantiate()
+	var spawn_point: Marker2D = new_scene.find_child("SpawnPoint")
+	if spawn_point == null:
+		print("No SpawnPoint found in the new scene %s" % scene.resource_name)
+		return
 	prev_scene = get_current_scene()
 	get_scene_holder().call_deferred("remove_child", get_current_scene())
 	player = get_tree().get_nodes_in_group("Player")[0]
 	# Offset player from the collider to prevent instantly switching the scene
 	prev_position = player.global_position + ((player.global_position - trigger.global_position)+Vector2(0,1))
-	var new_scene = scene.instantiate()
 	get_scene_holder().call_deferred("add_child",new_scene)
-	var spawn_point: Marker2D = new_scene.find_child("SpawnPoint")
 	player.set_deferred("global_position", spawn_point.global_position)
 	
 func to_previous():
