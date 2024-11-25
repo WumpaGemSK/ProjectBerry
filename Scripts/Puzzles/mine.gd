25a0f9bea8c882d9538fbd0b899cddc750a750ca
Joan Prim Armengol <jonpri1995@hotmail.com>
2024-11-25 12:14:18 +0100

    Mine object and reload scene when the mine's steps_left is 0 and is hit

---
+++ Scripts/Puzzles/mine.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@export var steps: int = 1
+
+var steps_left: int
+
+func _ready():
+	steps_left = steps
+
+# TODO: Fix ghost collision causing the mine to have one less step than what's expected
+func _on_area_2d_body_entered(body):
+	if body is Player:
+		if steps_left > 0:
+			steps_left -= 1
+		else:
+			body.take_damage(1)
+			SceneSwitcher.reload_scene()
--- Scripts/SceneSwitcher.gd
+++ Scripts/SceneSwitcher.gd
@@ -5,7 +5,17 @@ var prev_scene: Node = null
 var prev_position: Vector2
 var player: Player
 
+var packed_scenes: Array[PackedScene] = []
+
+func _ready():
+	player = get_tree().get_nodes_in_group("Player")[0]
+	prev_position = player.global_position
+	var pack = PackedScene.new()
+	pack.pack(get_current_scene())
+	packed_scenes.push_front(pack)
+
 func change_scene(scene: PackedScene, trigger: Node2D):
+	packed_scenes.push_front(scene)
 	var new_scene = scene.instantiate()
 	var spawn_point: Marker2D = new_scene.find_child("SpawnPoint")
 	if spawn_point == null:
@@ -13,7 +23,6 @@ func change_scene(scene: PackedScene, trigger: Node2D):
 		return
 	prev_scene = get_current_scene()
 	get_scene_holder().call_deferred("remove_child", get_current_scene())
-	player = get_tree().get_nodes_in_group("Player")[0]
 	# Offset player from the collider to prevent instantly switching the scene
 	prev_position = player.global_position + ((player.global_position - trigger.global_position)+Vector2(0,1))
 	get_scene_holder().call_deferred("add_child",new_scene)
@@ -34,3 +43,9 @@ func get_current_scene() -> Node:
 
 func get_scene_holder() -> Node:
 	return get_tree().root.get_child(-1).get_child(0)
+
+func reload_scene():
+	var new_scene = packed_scenes[0].instantiate()
+	player.global_position =  prev_position
+	get_scene_holder().call_deferred("remove_child", get_current_scene())
+	get_scene_holder().call_deferred("add_child", new_scene)
