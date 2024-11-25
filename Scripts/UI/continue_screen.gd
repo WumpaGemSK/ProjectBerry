95154283a9c513d743c89546d7c405c09435492d
Joan Prim Armengol <jonpri1995@hotmail.com>
2024-11-25 16:15:59 +0100

    Pause input for some time to let the player read the screen

--- Scripts/UI/continue_screen.gd
+++ Scripts/UI/continue_screen.gd
@@ -2,17 +2,24 @@ extends Control
 
 @onready var timer = $Timer
 @onready var tries = %Tries
-
+var pause_input = true
+var phase_in: Timer
 func _ready():
 	visible = false
 	timer.timeout.connect(on_timeout)
 	EventBus.pause.connect(on_pause)
-
+	phase_in = Timer.new()
+	add_child(phase_in)
+	phase_in.stop()
+	phase_in.timeout.connect(func(): pause_input=false)
 
 func _input(event):
+	if pause_input:
+		return
 	if visible and event.is_action_type():
 		visible = false
 		timer.stop()
+		pause_input = true
 		EventBus.resume.emit()
 		EventBus.retry_continue.emit()
 
@@ -25,6 +32,7 @@ func switch_to_game_over():
 func on_pause():
 	var tries_left = Game.retries
 	visible = true
+	phase_in.start(1)
 	if tries_left > 0:
 		tries.text = "%d tries left." % [tries_left]
 		timer.start()
