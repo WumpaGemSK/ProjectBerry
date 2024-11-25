31256bebf3369322d0069d82913eb8deeff8f102
Joan Prim Armengol <jonpri1995@hotmail.com>
2024-11-25 15:31:24 +0100

    Fix player being placed in the wrong location after a scene reload

--- Scripts/SceneSwitcher.gd
+++ Scripts/SceneSwitcher.gd
@@ -3,19 +3,14 @@ extends Node
 var prev_scene: Array[Node] = []
 
 var prev_position: Array[Vector2] = []
-var player: Player
+var player: Player = null
 
 var packed_scenes: Array[PackedScene] = []
 
-func _ready():
-	player = get_tree().get_nodes_in_group("Player")[0]
-	var pack = PackedScene.new()
-	pack.pack(get_current_scene())
-	packed_scenes.push_back(pack)
-	prev_scene.push_back(get_current_scene())
-	prev_position.push_back(player.global_position)
-
 func change_scene(scene: PackedScene, trigger: Node2D):
+	if player == null:
+		player = get_tree().get_nodes_in_group("Player")[0]
+	prev_position.push_back(player.global_position)
 	packed_scenes.push_back(scene)
 	var new_scene = scene.instantiate()
 	var spawn_point: Marker2D = new_scene.find_child("SpawnPoint")
@@ -28,6 +23,7 @@ func change_scene(scene: PackedScene, trigger: Node2D):
 	prev_position.push_back(player.global_position + ((player.global_position - trigger.global_position)+Vector2(0,1)))
 	get_scene_holder().call_deferred("add_child",new_scene)
 	player.set_deferred("global_position", spawn_point.global_position)
+	prev_position.push_back(spawn_point.global_position)
 	
 func to_previous():
 	if prev_scene.is_empty():
@@ -35,6 +31,7 @@ func to_previous():
 		return
 	get_scene_holder().call_deferred("remove_child", get_current_scene())
 	get_scene_holder().call_deferred("add_child", prev_scene.pop_back())
+	prev_position.pop_back()
 	player.set_deferred("global_position", prev_position.pop_back())
 
 func get_current_scene() -> Node:
@@ -46,5 +43,5 @@ func get_scene_holder() -> Node:
 	return get_tree().root.get_child(-1).get_child(0)
 
 func reload_scene():
-	player.global_position =  prev_position.back()
+	player.global_position = prev_position.back()
 	EventBus.reload_scene.emit()
