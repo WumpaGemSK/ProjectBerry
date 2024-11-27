3a8f95aed10b782d6497cc8f4e334f930c9412ef
Joan Prim Armengol <jonpri1995@hotmail.com>
2024-11-27 14:35:27 +0100

    Fixed error when no player on the scene

--- Scripts/Puzzles/Levels/level_storage_area.gd
+++ Scripts/Puzzles/Levels/level_storage_area.gd
@@ -6,5 +6,6 @@ extends Node2D
 func _ready():
 	puzzle_complete_collider.body_entered.connect(on_puzzle_completed)
 
-func on_puzzle_completed():
-	fax_machine.print_code()
+func on_puzzle_completed(body):
+	if body is Player:
+		fax_machine.print_code()
