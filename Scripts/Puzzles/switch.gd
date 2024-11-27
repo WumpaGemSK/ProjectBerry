c1f56c3d78380ec3af0b76abb567d166176eda12
Joan Prim Armengol <jonpri1995@hotmail.com>
2024-11-27 14:25:21 +0100

    Switchboard and computer sprite. Made the switch get the width from the region rect

--- Scripts/Puzzles/switch.gd
+++ Scripts/Puzzles/switch.gd
@@ -5,9 +5,11 @@ signal toggled(state: bool)
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @export var active: bool = false
+var sprite_width: int = 24
 
 func _ready():
-	sprite_2d.region_rect.position = Vector2(0, 0) if not active else Vector2(24, 0)
+	sprite_width = sprite_2d.region_rect.size.x
+	sprite_2d.region_rect.position = Vector2(0, 0) if not active else Vector2(sprite_width, 0)
 
 ## Toggle the state of the switch and return the current state
 func toggle() -> bool:
@@ -22,4 +24,4 @@ func reset():
 		update_sprite()
 
 func update_sprite():
-	sprite_2d.region_rect.position.x += 24 if active else -24
+	sprite_2d.region_rect.position.x += sprite_width if active else -sprite_width
