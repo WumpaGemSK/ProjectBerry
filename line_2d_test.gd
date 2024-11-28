289cda60f80de7d91327b8b192da3405d3db3fc0
Harris Devine <harisd@live.fr>
2024-11-28 18:48:50 +0100

    Updated Line2D test scene

--- line_2d_test.gd
+++ line_2d_test.gd
@@ -3,7 +3,6 @@ extends Node2D
 @onready var path_2d: Path2D = $Path2D
 @onready var path_follow_2d: PathFollow2D = $Path2D/PathFollow2D
 @onready var line_2d: Line2D = $Line2D
-@onready var marker_2d: Marker2D = $Path2D/PathFollow2D/Marker2D
 
 func _process(delta: float) -> void:
 	
@@ -14,4 +13,5 @@ func _process(delta: float) -> void:
 		var tween = create_tween()
 		tween.tween_property(path_follow_2d, "progress_ratio", 1, 10)
 		
-		line_2d.add_point(path_follow_2d.global_position)
+		var point = path_follow_2d.position
+		line_2d.add_point(point)
