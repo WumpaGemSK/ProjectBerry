7a32248cb74d618965e4fe6c17c1e793ddb30447
Harris Devine <harisd@live.fr>
2024-11-28 09:49:52 +0100

    Applied Transition Screen to Opening Logos

--- Scripts/UI/Start/game_off_2024.gd
+++ Scripts/UI/Start/game_off_2024.gd
@@ -4,9 +4,11 @@ extends Control
 
 func _ready() -> void:
 	
-	await get_tree().create_timer(3).timeout
+	await get_tree().create_timer(2).timeout
 	move_to_next_scene()
 
 func move_to_next_scene():
-	
+		
+	TransitionScreen.transition()
+	await TransitionScreen.on_transition_finished
 	get_tree().change_scene_to_packed(next_scene)
--- Scripts/UI/Start/shifty_kitty_logo.gd
+++ Scripts/UI/Start/shifty_kitty_logo.gd
@@ -4,4 +4,6 @@ extends Control
 
 func move_to_next_scene():
 	
+	TransitionScreen.transition()
+	await TransitionScreen.on_transition_finished
 	get_tree().change_scene_to_packed(next_scene)
