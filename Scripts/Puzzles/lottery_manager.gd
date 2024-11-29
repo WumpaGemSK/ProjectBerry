559fccd2543a677b915137bdbd5b5ca12f0fbece
Joan Prim Armengol <jonpri1995@hotmail.com>
2024-11-29 10:05:51 +0100

    Render line on the research lab and print the code when the correct flask has been picked

--- Scripts/Puzzles/lottery_manager.gd
+++ Scripts/Puzzles/lottery_manager.gd
@@ -3,9 +3,14 @@ extends Node
 signal puzzle_completed
 
 @export var correct_flask: Flask
+@onready var line_renderer = %LineRenderer
 
 func _ready():
 	correct_flask.interact.connect(on_interact)
+	line_renderer.animation_finished.connect(on_animation_finished)
 
 func on_interact():
+	line_renderer.render_line()
+
+func on_animation_finished():
 	puzzle_completed.emit()
