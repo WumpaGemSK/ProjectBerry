233be4836cf695165d6c6d2ab57e80106e8c3b1f
Joan Prim Armengol <jonpri1995@hotmail.com>
2024-11-29 16:47:30 +0100

    Art assets for terminal

--- Scripts/UI/code_input.gd
+++ Scripts/UI/code_input.gd
@@ -1,8 +1,8 @@
 extends Control
 class_name CodeInput
 
-@onready var code = %Code
-
+@onready var texture_rect = $MarginContainer/VBoxContainer/TextureRect
+const TERMINAL_LETTERS = preload("res://Assets/Textures/UI Screens/TerminalUI/terminal_letters.tres")
 var letter : String:
 	get():
 		return String.chr(code_point)
@@ -11,6 +11,7 @@ var code_point: int
 func _ready():
 	# Defaults to A
 	code_point = String("A").to_ascii_buffer()[0]
+	texture_rect.texture = TERMINAL_LETTERS.duplicate(true)
 	update_label()
 
 func _on_up_pressed():
@@ -26,4 +27,9 @@ func _on_down_pressed():
 	update_label()
 	
 func update_label():
-	code.text = letter
+	var width = 42
+	var height = 44
+	var offset = code_point - "A".to_ascii_buffer()[0]
+	var x = (offset % 7) * width
+	var y = floor(offset/7) * height
+	texture_rect.texture.region = Rect2(x, y, width, height)
