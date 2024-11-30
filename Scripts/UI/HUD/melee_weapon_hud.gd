c33fed895712116a2a352f9b9397e9c3335f7e06
Joan Prim Armengol <jonpri1995@hotmail.com>
2024-11-30 18:25:18 +0100

    Use higher res version for pistol and bat in the weapon slot

--- Scripts/UI/HUD/melee_weapon_hud.gd
+++ Scripts/UI/HUD/melee_weapon_hud.gd
@@ -1,7 +1,8 @@
 extends Control
 
 @onready var weapon_slot = $MarginContainer/WeaponSlot
-
+const CRICKET_BAT_BARBED_SLOT_ICON = preload("res://Assets/Textures/UI Screens/MainGameplayUI/ItemSlots/cricket_bat_barbed_slot_icon.png")
+const CRICKET_BAT_SLOT_ICON = preload("res://Assets/Textures/UI Screens/MainGameplayUI/ItemSlots/cricket_bat_slot_icon.png")
 ## Texture to show as the imput prompt
 @export var input_prompt : Texture2D
 
@@ -10,8 +11,11 @@ func _ready():
 	
 ## Callback to show the cricket bat texture and the input prompt
 func on_melee_weapon_equipped(item : Item):
-	if item.type != Item.Item_type.CRICKET_BAT:
-		return
-	weapon_slot.set_texture(item.texture_icon)
+	match item.type:
+		Item.Item_type.CRICKET_BAT:
+			weapon_slot.set_texture(CRICKET_BAT_SLOT_ICON)
+		Item.Item_type.BARBED_WIRE_UPGRADE:
+			weapon_slot.set_texture(CRICKET_BAT_BARBED_SLOT_ICON)
+		_: return
 	weapon_slot.set_input_prompt(input_prompt)
 	show()
--- Scripts/UI/HUD/ranged_weapon_hud.gd
+++ Scripts/UI/HUD/ranged_weapon_hud.gd
@@ -2,7 +2,7 @@ extends Control
 
 @onready var weapon_slot = $MarginContainer/VBoxContainer/WeaponSlot
 @onready var ammo_label = %AmmoLabel
-
+const PISTOL_SLOT_ICON = preload("res://Assets/Textures/UI Screens/MainGameplayUI/ItemSlots/pistol_slot_icon.png")
 ## Texture to show as the imput prompt
 @export var input_prompt : Texture2D
 
@@ -34,6 +34,6 @@ func update_ammo_label():
 func on_ranged_weapon_equipped(item : Item):
 	if item.type != Item.Item_type.PISTOL:
 		return
-	weapon_slot.set_texture(item.texture_icon)
+	weapon_slot.set_texture(PISTOL_SLOT_ICON)
 	weapon_slot.set_input_prompt(input_prompt, false)
 	show()
