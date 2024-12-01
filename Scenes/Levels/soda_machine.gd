7525d39b59b589eb0c27a4c100301bebe8bcc596
Harris Devine <harisd@live.fr>
2024-12-01 16:00:45 +0100

    Implemented Soda Machine secret

---
+++ Scenes/Levels/soda_machine.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var bag_of_coins: PickableItem = $BagofCoins
+
+func _ready() -> void:
+	
+	sprite.frame = 0
+
+func gets_destroyed():
+	
+	print("Destroyed")
+	sprite.frame = 1
+	bag_of_coins.position = Vector2(0, 20)
+	
+	
--- Scripts/Item/bullet.gd
+++ Scripts/Item/bullet.gd
@@ -12,6 +12,11 @@ func _on_timer_timeout():
 	queue_free()
 
 func _on_area_2d_body_entered(body):
+	
 	if body.is_in_group("Damageable"):
 		body.take_damage(damage)
+		
+	if body.is_in_group("SodaMachine"):
+		body.gets_destroyed()
+		
 	queue_free()
