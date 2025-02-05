20e8cb47039d2c179231f37b18d68eee67b54755
Joan Prim Armengol <jonpri1995@hotmail.com>
2025-02-05 12:11:37 +0100

    Add knockback to melee weapon

--- Scripts/melee_weapon.gd
+++ Scripts/melee_weapon.gd
@@ -3,9 +3,13 @@ extends Weapon
 func attack(from: Vector2, dest: Vector2):
 	if cooldown_timer.is_stopped():
 		var r = raycast_to_damageable(from, dest)
-		if r != null and r.is_in_group("Damageable"):
-			attacking.emit()
-			r.take_damage(damage)
+		if r.size() > 0:
+			var result = r.collider
+			if result != null and result.is_in_group("Damageable") and result.position.distance_to(from) < weapon_range:
+				attacking.emit()
+				var hitbox = result.get_node("HitboxComponent")
+				if hitbox:
+					hitbox.take_damage(damage, (-r.normal)*knockback_force)
 		cooldown_timer.start(cooldown)
 		AudioManager.play_effect(SoundEffect.SoundType.CRICKET_BAT_HIT)
 		
