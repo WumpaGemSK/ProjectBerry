27d1916b2038b0a06bb71a61c852e3a90b2bcff3
Joan Prim Armengol <jonpri1995@hotmail.com>
2025-02-06 18:07:17 +0100

    Ignore hit if invulnerability is active

--- Scripts/Components/hitbox_component.gd
+++ Scripts/Components/hitbox_component.gd
@@ -6,5 +6,7 @@ signal damage_taken(impact_dir: Vector2)
 @export var health_component : HealthComponent
 
 func take_damage(amount : int, impact_direction: Vector2 = Vector2.ZERO) -> void:
+	if health_component.invulnerable:
+		return
 	health_component.take_damage(amount)
 	damage_taken.emit(impact_direction)
